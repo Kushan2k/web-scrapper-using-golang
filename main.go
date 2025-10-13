d9c89a9c6fbc2d35f6acad8c2caa0d840482b446
Kushan Gayantha <kushangayantha001@gmail.com>
2025-10-13 21:18:03 +0530

    bug fixed

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/url"
 	"strings"
+	"sync"
+
+	"github.com/gocolly/colly"
 )
 
 
@@ -17,16 +22,58 @@ func main()  {
 	}
 	var to_find = make(map[string]string)
 
+	var results= make(chan map[string]string,10)
+	var wg sync.WaitGroup
+
+	
+
 	fmt.Print("Enter URL:- ")
 	fmt.Scan(&url)
 
+	suc,err:=isValidUrl(url)
+
+	if !suc {
+		log.Fatalln(err.Error())
+	}
+
 	generate_list(elements,&to_find)
 
-	
 
-	
-	
-	
+	c:=colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request){
+		fmt.Println("Visiting",r.URL.String())
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("👍 Done Loading the URL")
+	})
+
+	for key,val := range to_find {
+		wg.Add(1)
+
+		func(k, v string) { 
+			c.OnHTML(k, func(html *colly.HTMLElement) {
+				results <- map[string]string{v: html.Text}
+			})
+			wg.Done()
+		}(key, val)
+
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+	c.Visit(url)
+
+	for res := range results {
+		for k, v := range res {
+			fmt.Printf("%s: %s\n", k, v)
+		}
+	}
+
+
 	
 }
 
@@ -47,4 +94,18 @@ func generate_list(l map[string]string,to_find *map[string]string){
 	}
 
 
+}
+
+func isValidUrl(add string) (bool,error){
+
+	u,err:=url.ParseRequestURI(add)
+	if err!=nil{
+		return false,err
+	}
+
+	if u.Scheme=="" || u.Host==""{
+		return false,fmt.Errorf("invalid url")
+	}
+	return true,nil
+	
 }
